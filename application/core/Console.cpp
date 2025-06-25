0318a23e826720075be832a89d4c057e6748b5ae
Olav Ausland <olavausland@hotmail.com>
2025-06-25 19:22:27 +0200

    Improve console architecture

--- application/core/Console.cpp
+++ application/core/Console.cpp
@@ -1,27 +1,64 @@
 #include "Console.hpp"
-#include "Hierarchy.hpp"
 #include "imgui.h"
-#include "Script.hpp"
+#include "imgui_internal.h"
+#include <unordered_map>
+#include <vector>
+#include <iomanip>
+
+std::vector<Console::Message> Console::messages;
+
+void Console::AddMessage(Message message)
+{
+    std::time_t now = std::time(nullptr);
+    std::tm* time = std::localtime(&now);
+    char text[9];
+
+    std::snprintf(text, sizeof(text), "%02d:%02d:%02d", time->tm_hour, time->tm_min, time->tm_sec);
+
+    message.time = text;
+    messages.push_back(message);
+}
 
 void Console::Update()
 {
+    using namespace ImGui;
+
+    static std::unordered_map<MessageType, ImVec4> colors =
+    {
+        {MessageType::Error,    {1.0, 0.0, 0.0, 1.0}},
+        {MessageType::Warning,  {1.0, 0.5, 0.2, 1.0}},
+        {MessageType::Info,     {0.0, 1.0, 0.0, 1.0}}
+    };
+
     ImGui::Begin("Console");
 
-
-    for(auto const& entity : Hierarchy::GetEntities())
+    for(int i = 0; i < messages.size(); i++)
     {
-
-
-        for(auto const& script : entity.scripts)
+        const Message& message = messages.at(i);
+        ImGui::PushID(&message);
+        ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 4));
+        
+        if(ImGui::Button("Remove", {0, 0}))
         {
-            if(script->info_messages.size() > 0)
-                ImGui::TextColored({1.0, 0.0, 1.0, 1.0}, "%s", script->GetName().c_str());
-
-            for(auto const& message : script->info_messages)
-            {
-                ImGui::TextColored({1.0, 0.0, 0.0, 1.0}, "%s", message.c_str());
-            }
+            messages.erase(messages.begin() + i);
         }
+
+        ImGui::SameLine();
+
+        ImGui::TextColored(colors[message.type], "%s", message.title.c_str());
+        
+        ImGui::SameLine();
+        
+        ImGui::SetCursorPosX(GetCursorPosX() + ImMax(0.0f, GetContentRegionAvail().x - CalcTextSize(message.title.c_str()).x));
+        ImGui::Text("%s", message.time.c_str());
+
+        ImGui::BeginChild("ErrorMessages", ImVec2(-1, 50));
+
+        ImGui::Text("%s", message.content.c_str());
+
+        ImGui::EndChild();
+        ImGui::PopStyleVar();
+        ImGui::PopID();
     }
 
     ImGui::End();
--- application/core/Console.hpp
+++ application/core/Console.hpp
@@ -1,7 +1,30 @@
 #pragma once
+#include <string>
+#include <vector>
+
 
 class Console
 {
 public:
+    enum class MessageType
+    {
+        Info,
+        Warning,
+        Error
+    };
+
+    typedef struct
+    {
+        MessageType type;
+        std::string title;
+        std::string content;
+        std::string time;
+    } Message;
+
+    static std::vector<Message> messages;
+
+public:
+    static void AddMessage(Message message);
     static void Update();
 };
+
